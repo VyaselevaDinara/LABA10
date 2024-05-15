25581bd201e1265a1846636db98f7bd99bc7e623
Вяселева Динара <118040854+VyaselevaDinara@users.noreply.github.com>
2024-05-15 09:20:43 +0300

    Add files via upload

---
+++ лабаWS№10.cs
@@ -0,0 +1,40 @@
+﻿class EventClass
+{
+    public event Action<string> Event;
+
+    public string Name { get; set; }
+
+    public EventClass(string name)
+    {
+        Name = name;
+    }
+
+    public void RaiseEvent()
+    {
+        Event?.Invoke(Name);
+    }
+}
+
+class HandlerClass
+{
+    public void HandleEvent(string name)
+    {
+        Console.WriteLine($"Событие - {name}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EventClass obj1 = new EventClass("Объект 1");
+        EventClass obj2 = new EventClass("Объект 2");
+        HandlerClass handler = new HandlerClass();
+
+        obj1.Event += handler.HandleEvent;
+        obj2.Event += handler.HandleEvent;
+
+        obj1.RaiseEvent();
+        obj2.RaiseEvent();
+    }
+}
